7d703f5c02ba53f79538e373a1b662571bc10dd5
Eugene V <eugenevasile@gmail.com>
2024-11-16 13:55:22 +0100

    Changes in configuration files

--- flsk-customer-env/app.py
+++ flsk-customer-env/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request
-from markupsafe import escape
+# from flask import Flask, request
+# from markupsafe import escape
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/status", methods=['GET'])
-def status():
-    return "<p>OK</p>"
+# @app.route("/status", methods=['GET'])
+# def status():
+#     return "<p>OK</p>"
 
-@app.route("/customer/<id>", methods=['GET'])
-def show_customer_profile(id: int):
-    pass
+# @app.route("/customer/<id>", methods=['GET'])
+# def show_customer_profile(id: int):
+#     pass
