e691abee19bf096219e6dbbbb75809cf22e42b9f
Eugene V <eugenevasile@gmail.com>
2024-11-17 10:18:39 +0100

    added comments

--- flsk-customer-env/db/connection.py
+++ flsk-customer-env/db/connection.py
@@ -1,11 +1,18 @@
 import sqlite3
 from flask import g
 
+
 class DBConnection:
 
     DATABASE = 'data/customer.db'
 
     def get_db(self):
+
+        """ Opens the connection to SQLite database.
+
+        Returns:
+            connection: A valid SQLite connection. 
+        """        
         
         db = getattr(g, '_database', None)
         
@@ -13,8 +20,10 @@ class DBConnection:
             db = g._database = sqlite3.connect(self.DATABASE)
             
         return db
-    
+
     def close_connection(self, conn):
 
+        """ Closes the Db connection.
+        """     
         if conn is not None:
             conn.close()
